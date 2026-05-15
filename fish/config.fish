0a56eb115b1d0ea2cd9de0461f8e9d5bc197e942
Zach Brown <bbrown64506@gmail.com>
2026-05-15 14:20:52 -0500

    feat: copy over bulk of dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,30 @@
+set fish_greeting
+
+if status is-interactive
+# Commands to run in interactive sessions can go here
+  # -- Starship --
+  starship init fish | source
+  enable_transience
+
+  # -- lazygit --
+  export XDG_CONFIG_HOME="$HOME/.config"
+
+  # -- Nix --
+  abbr -a drs "sudo darwin-rebuild switch"
+
+  # -- zoxide --
+  abbr -a cd z
+  zoxide init fish | source
+
+  # -- eza --
+  abbr -a ls "eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+
+  # -- bat --
+  abbr -a cat bat
+
+  # -- fzf --
+  fzf --fish | source
+
+  # -- fastfetch --
+  abbr -a ff fastfetch
+end

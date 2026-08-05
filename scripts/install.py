bdb9e374ac888d225ef1399c44dedba2f2d32a91
Zach Brown <bbrown64506@gmail.com>
2026-08-05 16:50:17 -0500

    feat: remove metapac

--- scripts/install.py
+++ scripts/install.py
@@ -3,31 +3,12 @@
 import sys
 
 from lib.common import logger, stow_config, decrypt_ssh_key
-from lib.metapac import metapac_exists, metapac_install, metapac_link
-from lib.rust import cargo_exists, rust_install
-from lib.metapac import metapac_sync
 
 
 def install():
     logger.info("Installing dotfiles")
-    if not cargo_exists():
-        rust_install()
-    else:
-        logger.info("cargo already installed... skipping.")
-
-    if not metapac_exists():
-        metapac_install()
-    else:
-        logger.info("metapac already installed... skipping.")
-
-    metapac_link()
-
-    metapac_sync()
-
     stow_config()
-
     decrypt_ssh_key()
-
     logger.info("Installation complete!.")
 
 
